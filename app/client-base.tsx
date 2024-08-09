332d28746edddca90c6b08682830dfb45a3d173d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-09 21:31:26 +1200

    feat: set the router's preload setting to 'intent'

--- app/client-base.tsx
+++ app/client-base.tsx
@@ -5,7 +5,10 @@ import { routeTree } from "./routeTree.gen";
 import "./client-base.css";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+	routeTree,
+	defaultPreload: "intent",
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
